63fffcb8bad114dcf10fe1977d9268615a9126ab
liannep <liannebip2001@yahoo.com>
2018-03-14 14:12:14 +1300

    Initial commit

---
+++ src/main/java/MySecondMavenProject/App.java
@@ -0,0 +1,13 @@
+package MySecondMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
